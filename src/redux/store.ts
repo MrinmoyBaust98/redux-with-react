877eac35e3483203960c6fa32c2fd3d26a2d032b
Mrinmoy Kumer Ray <baust.cse.170201073@gmail.com>
2023-07-06 04:18:39 +0600

    [Done] middleware [logger] implement & cumtom middleware[custom logger] added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counter/counterSlice";
+import logger from "./middleware/logger";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
